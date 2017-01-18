8c5a6b77f0a302c4d7f8ed6e1c041714b2607301
Maciej Kobus <maciej@mkobus.com>
2017-01-18 12:58:09 +0100

    Implemented Template Block using new YAML definition method

--- Block/TemplateBlock.php
+++ Block/TemplateBlock.php
@@ -2,16 +2,14 @@
 namespace EzSystems\LandingPageBlockTemplateBundle\Block;
 
 use EzSystems\LandingPageFieldTypeBundle\Exception\InvalidBlockAttributeException;
-use EzSystems\LandingPageFieldTypeBundle\FieldType\LandingPage\Definition\BlockDefinition;
-use EzSystems\LandingPageFieldTypeBundle\FieldType\LandingPage\Definition\BlockAttributeDefinition;
-use EzSystems\LandingPageFieldTypeBundle\FieldType\LandingPage\Model\AbstractBlockType;
+use EzSystems\LandingPageFieldTypeBundle\FieldType\LandingPage\Model\AbstractConfigurableBlockType;
 use EzSystems\LandingPageFieldTypeBundle\FieldType\LandingPage\Model\BlockValue;
 
 /**
  * Template block
  * Template for Landing Page Field Type Block.
  */
-class TemplateBlock extends AbstractBlockType
+class TemplateBlock extends AbstractConfigurableBlockType
 {
     /**
      * Returns the parameters to the template.
@@ -24,41 +22,6 @@ class TemplateBlock extends AbstractBlockType
         return [
             'block' => json_encode($blockValue->getAttributes())
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createBlockDefinition()
-    {
-        return new BlockDefinition(
-            'template', // Block type (unique)
-            'Template', // Name of block
-            'default', // Block category (currently unsupported)
-            'bundles/ezsystemslandingpageblocktemplate/images/templateblock.svg', // icon for thumbnail
-            [], // extra views that can be hardcoded
-            [
-                new BlockAttributeDefinition(
-                    'templateContent', // Attribute's ID (unique)
-                    'Content', // Attribute' name
-                    'text', // Attribute's type
-                            // Available options:
-                            // - integer
-                            // - string
-                            // - url
-                            // - text
-                            // - embed
-                            // - select
-                            // - multiple
-                    '/[^\\s]/', // regex for frontend validation
-                    'Sample content', // regex validation fail message
-                    true, // is field required?
-                    false, // should this attribute input be displayed inline to the previous?
-                    [], // default value
-                    [] // available options (for select and multiple)
-                ),
-            ]
-        );
     }
 
     /**
